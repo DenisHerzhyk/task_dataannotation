1981bdc4b98b444024e20e211c0f86006cf5bd98
Denys Herzhyk <102976544+DenisHerzhyk@users.noreply.github.com>
2024-02-15 19:13:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
-text = ["3 love",
-        "6 computers",
-        "2 dogs",
-        "4 cats",
-        "1 I",
-        "5 you"]
+with open('text.txt', 'r') as file:
+    text = file.readlines()
 numbers = []
-levels_array = []
 id_numbers = []
+step = 2
+index = 0
 text = sorted(text, key=lambda x: int(x.split()[0]))
 
 for elem in text:
-    for symbol in elem:
-        if symbol.isdigit():
-            numbers.append(int(symbol))
+    numbers.append(int(elem.split()[0]))
 
-levels = len(numbers) // 2
-for i in range(1, levels + 1):
-    line = " " * (levels - i)
-    for _ in range(i):
-        line += str(numbers.pop(0)) + " "
-    line += " " * (levels - i - 1)
-    if not numbers:
-        line = line[:-1]
-    levels_array.append(line)
+for number in numbers:
+    id_numbers.append(numbers[index])
+    index += step
+    step += 1
+    if index >= len(numbers):
+        index = len(numbers) - 1
+        id_numbers.append(numbers[index])
+        break
 
-for i in levels_array:
-    for j in reversed(i):
-        if j.isdigit():
-            id_numbers.append(j)
-            break
-
-for i in text:
-    for j in i:
-        if j in id_numbers:
-            print(i)
+for line in text:
+    if (int(line.split()[0]) in id_numbers):
+        print(line)
